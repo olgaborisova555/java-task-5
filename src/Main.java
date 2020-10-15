72bfdb53ff27d58a2606e5bfbf25620563334dd8
olgaborisova <olga.borisova803@gmail.com>
2020-10-16 01:29:08 +0300

    add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 59;
+        int milesPrice = 20;
+        int miles;
+
+        miles = ticketPrice/milesPrice;
+        System.out.println("Вам начислено " + miles +  " миль");
+    }
+}
